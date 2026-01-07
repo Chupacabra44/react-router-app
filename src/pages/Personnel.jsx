32e0bb1b42ecae721f814a24016b94fb575333b1
Sanja Daic <sanja.daic86@gmail.com>
2026-01-07 14:05:34 +0100

    Personnel Page

--- src/pages/Personnel.jsx
+++ src/pages/Personnel.jsx
@@ -1,5 +1,35 @@
+import { useEffect, useState } from "react";
+
 const Personnel = () => {
-  return <div>Personnel</div>;
+  const [people, setPeople] = useState([]);
+
+  const getPeople = () => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((data) => setPeople(data))
+      .catch((error) => console.log(error));
+  };
+
+  useEffect(() => {
+    getPeople();
+  }, []);
+
+  return (
+    <div className="personnelWrapper">
+      <h1>Personnel List</h1>
+      <div className="cardWrapper">
+        {people.map((person) => {
+          const { id, name, address } = person;
+          return (
+            <div className="personnelCard" key={id}>
+              <p>{name}</p>
+              <p>{address.city}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Personnel;
