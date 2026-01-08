6685730b762d6deb1907c5d8e06455b354da41d5
Sanja Daic <sanja.daic86@gmail.com>
2026-01-08 10:58:29 +0100

    Loading

--- src/pages/PersonnelDetail.jsx
+++ src/pages/PersonnelDetail.jsx
@@ -5,6 +5,7 @@ import NotFound from "./NotFound";
 const PersonnelDetail = () => {
   const [person, setPerson] = useState("");
   const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const navigate = useNavigate();
   const { id } = useParams();
@@ -16,12 +17,16 @@ const PersonnelDetail = () => {
       .then((response) => {
         if (!response.ok) {
           setError(true);
+          setLoading(false);
           throw new Error("Something went wrong!");
         }
         return response.json();
       })
 
-      .then((data) => setPerson(data))
+      .then((data) => {
+        setLoading(false);
+        setPerson(data);
+      })
       .catch((error) => console.log(error));
   };
 
@@ -29,15 +34,25 @@ const PersonnelDetail = () => {
     getPerson();
   }, []);
 
-  console.log(person);
+  // console.log(person);
 
   if (error) {
     return <NotFound />;
-  } else {
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <h3>Data Loading ...</h3>
+      </div>
+    );
+  }
+
+  if (!error && !loading) {
     return (
       <div className="personWrapper">
         <h3>{person?.name}</h3>
-        <p>{person?.address?.city}</p>
+        <p>{person?.address.city}</p>
         <div className="btnWrapper">
           <button onClick={() => navigate("/")}>Home</button>
           <button onClick={() => navigate("/personnel")}>Go Back</button>
